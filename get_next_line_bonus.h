10dbfe46de3f99de6ed2aaa8ea4c6816aecb369f
M-Jorda <jorda.j.fr@gmail.com>
2024-11-23 15:57:03 +0100

    gnl

---
+++ get_next_line_bonus.h
@@ -0,0 +1,38 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 11:20:52 by jjorda            #+#    #+#             */
+/*   Updated: 2024/11/07 12:34:47 by jjorda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
+
+# include <stdlib.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+
+/* ----------------------------- get_next_line ------------------------------ */
+
+char	*get_next_line_fd(int fd);
+char	*ft_free(char **str);
+
+/* -------------------------- get_next_line_utils --------------------------- */
+
+char	*ft_strdup(char *s1);
+size_t	ft_strlen(const char *s);
+char	*ft_substr(const char *s, unsigned int start, size_t len);
+char	*ft_gnl_strjoin(const char *s1, const char *s2);
+int		ft_gnl_strchr(char *s, int c);
+
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,105 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils_bonus.c                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jjorda <jjorda@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/26 11:48:03 by jjorda            #+#    #+#             */
+/*   Updated: 2024/11/05 21:12:53 by jjorda           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+char	*ft_strdup(char *s1)
+{
+	char	*dest;
+	int		i;
+	int		j;
+
+	i = 0;
+	j = 0;
+	while (s1[i])
+		i++;
+	dest = (char *)malloc(sizeof(char) * (i + 1));
+	if (dest == NULL)
+		return (NULL);
+	while (s1[j])
+	{
+		dest[j] = s1[j];
+		j++;
+	}
+	dest[j] = '\0';
+	return (dest);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_substr(const char *s, unsigned int start, size_t len)
+{
+	unsigned int	i;
+	char			*substr;
+	unsigned int	s_len;
+
+	if (!s)
+		return (NULL);
+	s_len = (unsigned int) ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (start + len > s_len)
+		len = s_len - start;
+	substr = (char *) malloc(sizeof(char) * (len + 1));
+	if (!substr)
+		return (NULL);
+	i = -1;
+	while (++i < (unsigned int)len)
+		substr[i] = s[start + i];
+	substr[i] = '\0';
+	return (substr);
+}
+
+char	*ft_gnl_strjoin(const char *s1, const char *s2)
+{
+	int		i;
+	char	*result;
+	int		len_s1;
+	int		len_s2;
+
+	len_s1 = ft_strlen(s1);
+	len_s2 = ft_strlen(s2);
+	i = len_s1 + len_s2;
+	result = (char *) malloc(sizeof(char) * (i + 1));
+	if (!result)
+		return (ft_free((char **) &s1));
+	i = -1;
+	while (++i < len_s1)
+		result[i] = s1[i];
+	i = -1;
+	while (++i < len_s2)
+		result[i + len_s1] = s2[i];
+	result[i + len_s1] = '\0';
+	ft_free((char **) &s1);
+	return (result);
+}
+
+int	ft_gnl_strchr(char *s, int c)
+{
+	unsigned int	i;
+
+	if (!s)
+		return (0);
+	i = -1;
+	while (s[++i])
+		if (s[i] == (char) c)
+			return (i + 1);
+	return (0);
+}
